16dd1e5aff5512dd65997d2f2bd556a52c83b1e6
jacob-abe <momrider69@gmail.com>
2021-10-27 17:42:21 +0530

    Finished minting code

--- src/App.js
+++ src/App.js
@@ -1,31 +1,109 @@
-import './styles/App.css';
-import twitterLogo from './assets/twitter-logo.svg';
-import React from "react";
+import './styles/App.css'
+import twitterLogo from './assets/twitter-logo.svg'
+import React, { useEffect, useState } from 'react'
+import { ethers } from "ethers";
+import abi from "./utils/NFTContract.json"
 
 // Constants
-const TWITTER_HANDLE = 'momrider_69';
-const TWITTER_LINK = `https://jacobabe.bio.link`;
-const OPENSEA_LINK = '';
-const TOTAL_MINT_COUNT = 50;
+const TWITTER_HANDLE = 'momrider_69'
+const TWITTER_LINK = `https://jacobabe.bio.link`
+const OPENSEA_LINK = ''
+const TOTAL_MINT_COUNT = 50
 
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState('')
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window
+    if (!ethereum) {
+      console.log('Make sure you have metamask!')
+      return
+    } else {
+      console.log('We have the ethereum object', ethereum)
+    }
+    const accounts = await ethereum.request({ method: 'eth_accounts' })
+    if (accounts.length !== 0) {
+      const account = accounts[0]
+      console.log('Found an authorized account:', account)
+      setCurrentAccount(account)
+    } else {
+      console.log('No authorized account found')
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window
+
+      if (!ethereum) {
+        alert('Get MetaMask!')
+        return
+      }
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
+      console.log('Connected', accounts[0])
+      setCurrentAccount(accounts[0])
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const askContractToMintNft = async () => {
+    const CONTRACT_ADDRESS = "0xf8C3b055E2C774C01fa2f658D78a56B6eF313B60";
+      try {
+        const { ethereum } = window;
+  
+        if (ethereum) {
+          const provider = new ethers.providers.Web3Provider(ethereum);
+          const signer = provider.getSigner();
+          const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, abi.abi, signer);
+  
+          console.log("Going to pop wallet now to pay gas...")
+          let nftTxn = await connectedContract.mintNFT();
+  
+          console.log("Mining...please wait.")
+          await nftTxn.wait();
+          
+          console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+  
+        } else {
+          console.log("Ethereum object doesn't exist!");
+        }
+      } catch (error) {
+        console.log(error)
+      }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected()
+  }, [])
   // Render Methods
   const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
+    <button
+      onClick={connectWallet}
+      className="cta-button connect-wallet-button"
+    >
       Connect to Wallet
     </button>
-  );
+  )
+
+  const renderConnectedContainer = () => (
+    <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+      Mint NFT
+    </button>
+  )
 
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">Questionable Quotes NFT Collection</p>
+          <p className="header gradient-text">
+            Questionable Quotes NFT Collection
+          </p>
           <img src="https://vcvoices.org/wp-content/uploads/2015/04/c8129f9ce.jpg"></img>
           <p className="sub-text">
             Grab your unique NFT today and display it to the world.
           </p>
-          {renderNotConnectedContainer()}
+          {currentAccount ? renderConnectedContainer() : renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
@@ -38,7 +116,7 @@ const App = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
